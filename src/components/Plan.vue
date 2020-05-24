34b64ed2e3e06d743c4df3693ddb23739d1400c6
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-23 22:19:32 -0400

    Adicionar un método para seleccionar y un buen ejemplo de un binding

--- src/components/Plan.vue
+++ src/components/Plan.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <div class="plan">
+      <div @click="seleccionar" class="plan" :class="{'plan-activo':seleccionado}">
         <div class="descripcion">
           <span class="titulo">{{ nombre }}</span>
         </div>
@@ -17,7 +17,16 @@
                 default: 'Plan 1 - Principiante',
                 required: true
             },
-            precio: Number
+        },
+        data () {
+          return {
+            seleccionado: false
+          }
+        },
+        methods: {
+          seleccionar(){
+            this.seleccionado = true;
+          }
         }
     }
 </script>
